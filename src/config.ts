4884a220a42d866b4e19a472f5356c98c557a5ee
Arseniy <102667061+AsterYnG@users.noreply.github.com>
2026-02-08 23:25:59 +0300

    BZR-5 removed /api suffix from configs (#14)

--- src/config.ts
+++ src/config.ts
@@ -1,49 +1,49 @@
 // src/config.ts
 
 interface Config {
-  api: {
-    baseUrl: string;
-    targetLocal: string;
-  };
-  gatewayApi: {
-    baseUrl: string;
-    targetLocal: string;
-  };
-  personaApi: {
-    baseUrl: string;
-    targetLocal: string;
-  };
-  chatApi: {
-    baseUrl: string;
-    targetLocal: string;
-  };
-  auth: {
-    keycloakUrl: string;
-    targetLocal: string;
-  };
+    api: {
+        baseUrl: string;
+        targetLocal: string;
+    };
+    gatewayApi: {
+        baseUrl: string;
+        targetLocal: string;
+    };
+    personaApi: {
+        baseUrl: string;
+        targetLocal: string;
+    };
+    chatApi: {
+        baseUrl: string;
+        targetLocal: string;
+    };
+    auth: {
+        keycloakUrl: string;
+        targetLocal: string;
+    };
 }
 
 const config: Config = {
-  api: {
-    baseUrl: '/api/bazar-space/api',
-    targetLocal: '/bazar-space/api' // локальный json-server через vite proxy
-  },
-  personaApi: {
-    baseUrl: '/api/bazar-persona/api',
-    targetLocal: '/bazar-persona/api' // локальный json-server через vite proxy
-  },
-  gatewayApi: {
-    baseUrl: '/api/api',
-    targetLocal: '/api' // локальный json-server через vite proxy
-  },
-  chatApi: {
-    baseUrl: '/api/bazar-chat/api',
-    targetLocal: '/bazar-chat/api' // локальный json-server через vite proxy
-  },
-  auth: {
-    keycloakUrl: '/api/oauth2/authorization/keycloak',
-      targetLocal: '/oauth2/authorization/keycloak' // локальный mock
-  },
+    api: {
+        baseUrl: '/api/bazar-space',
+        targetLocal: '/bazar-space' // локальный json-server через vite proxy
+    },
+    personaApi: {
+        baseUrl: '/api/bazar-persona',
+        targetLocal: '/bazar-persona' // локальный json-server через vite proxy
+    },
+    gatewayApi: {
+        baseUrl: '/api',
+        targetLocal: '/' // локальный json-server через vite proxy
+    },
+    chatApi: {
+        baseUrl: '/api/bazar-chat',
+        targetLocal: '/bazar-chat' // локальный json-server через vite proxy
+    },
+    auth: {
+        keycloakUrl: '/api/oauth2/authorization/keycloak',
+        targetLocal: '/oauth2/authorization/keycloak' // локальный mock
+    },
 };
 
 export default config;
