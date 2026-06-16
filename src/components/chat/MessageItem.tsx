4c5bcc293e543f7ae1953adcb5f5fc63df1b207d
paul <kulebyakinpaul@yandex.ru>
2026-06-16 21:15:55 +0300

    BZR-74 Small fix

--- src/components/chat/MessageItem.tsx
+++ src/components/chat/MessageItem.tsx
@@ -160,7 +160,7 @@ export const MessageItem = ({
                       <Reply className="h-3 w-3" />
                       <span className="font-medium">{replyAuthorDisplayName}</span>
                     </div>
-                    <p>
+                    <p className="truncate">
                         {message.reply.contentPreview}
                         {message.reply.contentPreview.length >= 30 ? '...' : ''}
                     </p>
